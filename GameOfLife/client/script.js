f8b374de374bac1920721e71ca68a575262304f0
marimkrt09 <mariam.mkrtchyan5@tumo.org>
2023-11-19 15:14:01 +0400

    Add Event

--- GameOfLife/client/script.js
+++ GameOfLife/client/script.js
@@ -1,35 +1,56 @@
 let socket = io()
 let side = 30
+let grasscolor = "green"
+
+
+let light = document.getElementById("Light");
+light.addEventListener("click", lighting);
+
+function lighting(){
+        grasscolor = "orange"
+        new Audio("./natural-thunder-113219.mp3").play();
+
+}
 
 
 function setup() {
         createCanvas(30 * side, 30 * side)
         background("#acacac");
+         
 }
 
 
-function nkarel(matrix) {
+function changeColor(matrix) {
         for (let y = 0; y < matrix.length; y++) {
                 for (let x = 0; x < matrix[y].length; x++) {
                         let obj = matrix[y][x]
-                        if (obj == 1) {
-                                fill("green")
-                        } else if(obj == 2){
+                        let toBot = side - side * 0.5
+                        if (obj == 1){
+                                fill(grasscolor)
+                                rect(x * side, y * side, side, side)
+                                text('🍀', x * side, y * side + toBot)
+                        }else if(obj == 2){
                                 fill ("yellow") 
+                                rect(x * side, y * side, side, side)
                         }else if(obj == 3){
                                 fill ("red")
+                                rect(x * side, y * side, side, side)
                         }else if(obj == 4){
                                 fill("white")
+                                rect(x * side, y * side, side, side)
                         }else if(obj == 5){
                                 fill ("cyan")
+                                rect(x * side, y * side, side, side)
                         }else if(obj == 6){
                                 fill ("navy")
+                                rect(x * side, y * side, side, side)
                         }else if(obj == 7){
                                 fill("black")
+                                rect(x * side, y * side, side, side)
                         }else {
                                 fill("gray")
+                                rect(x * side, y * side, side, side)
                         }
-                        rect(x * side, y * side, side, side)
                 }
         }
 }
@@ -40,3 +61,5 @@ setInterval(
         socket.on('send matrix', nkarel)
         },400
     )
+
+
